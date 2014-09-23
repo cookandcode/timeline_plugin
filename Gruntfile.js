14812f84aad585c9bb301805a4995976349fddc1
Georges <georges@PC-de-G.local>
2014-09-23 12:23:01 +0200

    add grunt into project

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    uglify: {
+      my_target: {
+        files: {
+          'dist/js/timeline.min.js': ['src/js/svg.js', 'src/js/branch.js', 'src/js/timeline.js', 'src/js/plugin.js']
+        }
+      }
+    },
+    cssmin: {
+      combine: {
+        files: {
+          'dist/css/timeline.min.css': ['src/css/*.css']
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify', "cssmin"]);
+
+};
